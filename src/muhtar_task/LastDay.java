f573a6b9fef614e33789acff0ec9b8c42e65a65f
Edison Kashari <edisonkashari@hotmail.com>
2022-01-12 18:31:06 -0500

    second commit

--- src/muhtar_task/LastDay.java
+++ src/muhtar_task/LastDay.java
@@ -3,5 +3,6 @@ package muhtar_task;
 public class LastDay {
     public static void main(String[] args) {
         System.out.println("Edison");
+        System.out.println("Hello World");
     }
 }
